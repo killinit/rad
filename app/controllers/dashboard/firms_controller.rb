a4ff1f7bb477c09072f22f83274b54e368afca2a
Caden Lovelace <caden@herostrat.us>
2015-05-26 17:12:22 +0100

    [WIP] Add a firm edit page proper-like

--- app/controllers/dashboard/firms_controller.rb
+++ app/controllers/dashboard/firms_controller.rb
@@ -11,10 +11,24 @@ module Dashboard
       @firm = Firm.find(params[:id])
     end
 
+    def update
+      @firm = Firm.find(params[:id])
+      if @firm.update(firm_params)
+        flash[:notice] = 'A success message'
+      end
+      render :edit
+    end
+
     private
 
     def principal
       current_user.principal
+    end
+
+    def firm_params
+      params.require(:firm).permit(
+        :email_address
+      )
     end
   end
 end
--- spec/features/dashboard/firms_edit_spec.rb
+++ spec/features/dashboard/firms_edit_spec.rb
@@ -9,6 +9,23 @@ RSpec.feature 'The dashboard firm edit page' do
     when_i_am_on_the_principal_dashboard_firms_page
     and_i_click_the_edit_link_for_my_firm
     then_i_see_the_edit_page_for_my_firm
+    when_i_change_the_information
+    and_i_click_save
+    then_i_see_a_success_notice
+    and_the_information_is_changed
+  end
+
+  scenario 'The principal can edit their firm' do
+    given_i_am_a_fully_registered_principal_user
+    and_i_have_a_firm
+    and_i_am_logged_in
+    when_i_am_on_the_principal_dashboard_firms_page
+    and_i_click_the_edit_link_for_my_firm
+    then_i_see_the_edit_page_for_my_firm
+    when_i_invalidate_the_information
+    and_i_click_save
+    then_i_see_validation_messages
+    and_the_information_is_not_changed
   end
 
   def given_i_am_a_fully_registered_principal_user
@@ -19,6 +36,7 @@ RSpec.feature 'The dashboard firm edit page' do
   def and_i_have_a_firm
     firm_attrs = FactoryGirl.attributes_for(:firm, fca_number: @principal.fca_number)
     @principal.firm.update_attributes(firm_attrs)
+    @original_firm_email = @principal.firm.email_address
   end
 
   def and_i_am_logged_in
@@ -37,5 +55,37 @@ RSpec.feature 'The dashboard firm edit page' do
   def then_i_see_the_edit_page_for_my_firm
     expect(firm_edit_page).to be_displayed
     expect(firm_edit_page.firm_name).to have_text @principal.firm.registered_name
+  end
+
+  def when_i_change_the_information
+    firm_edit_page.firm_email_field.set 'i_dunno@example.com'
+  end
+
+  def when_i_invalidate_the_information
+    firm_edit_page.firm_email_field.set 'clearly_not_a_valid_email!'
+  end
+
+  def and_i_click_save
+    firm_edit_page.save_button.click
+  end
+
+  def then_i_see_a_success_notice
+    expect(firm_edit_page).to have_flash_message(text: 'A success message')
+  end
+
+  def then_i_see_validation_messages
+    expect(firm_edit_page).to have_validation_summary
+  end
+
+  def and_the_information_is_changed
+    expect(firm_edit_page.firm_email_field.value).to eq 'i_dunno@example.com'
+    @principal.reload
+    expect(@principal.firm.email_address).to eq 'i_dunno@example.com'
+  end
+
+  def and_the_information_is_not_changed
+    expect(firm_edit_page.firm_email_field.value).to eq 'clearly_not_a_valid_email!'
+    @principal.reload
+    expect(@principal.firm.email_address).to eq @original_firm_email
   end
 end
