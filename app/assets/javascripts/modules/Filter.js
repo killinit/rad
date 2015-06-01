5fd8ded63fb3874ab06194539b137d2ae3f18872
Caden Lovelace <caden@herostrat.us>
2015-06-01 16:12:55 +0100

    [Squash] Filter.js style fixes

--- app/assets/javascripts/modules/Filter.js
+++ app/assets/javascripts/modules/Filter.js
@@ -1,12 +1,10 @@
 define(['jquery', 'List', 'ListFuzzySearch'], function($, List, ListFuzzySearch) {
-
   'use strict';
 
   var firmOptions = {
-    valueNames: [ 'js-frn', 'js-firm-name', 'js-principal-name' ],
-    plugins: [ ListFuzzySearch() ]
+    valueNames: ['js-frn', 'js-firm-name', 'js-principal-name'],
+    plugins: [ListFuzzySearch()]
   };
 
   var firmList = new List('firm-list', firmOptions);
-
-} );
+});
